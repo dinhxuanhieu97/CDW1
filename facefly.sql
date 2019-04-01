1cdf80d3ee48703701757ee2aa7147ba90828691
Xuân Hiêu <dxhieu97@gmail.com>
2019-04-01 23:21:56 +0700

    master

--- facefly.sql
+++ facefly.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 13, 2019 at 02:23 AM
--- Server version: 5.6.30
--- PHP Version: 5.6.21
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 01, 2019 lúc 04:14 CH
+-- Phiên bản máy phục vụ: 5.7.14
+-- Phiên bản PHP: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,143 +17,77 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `facefly`
+-- Cơ sở dữ liệu: `facefly`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `airlines`
+-- Cấu trúc bảng cho bảng `airways`
 --
 
-CREATE TABLE IF NOT EXISTS `airlines` (
-  `airline_id` int(11) NOT NULL,
-  `airline_name` varchar(55) NOT NULL,
-  `airline_nation_id` int(11) DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+CREATE TABLE `airways` (
+  `id` int(11) NOT NULL,
+  `airways_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `airlines`
+-- Đang đổ dữ liệu cho bảng `airways`
 --
 
-INSERT INTO `airlines` (`airline_id`, `airline_name`, `airline_nation_id`) VALUES
-(1, 'Qatar Airways', 1),
-(2, 'Vietnam Airlines', 1),
-(3, 'VietJet', 3),
-(4, 'Japan Airlines', 3);
+INSERT INTO `airways` (`id`, `airways_name`) VALUES
+(1, 'Quatar Airlines'),
+(2, 'Vietnam Airlines'),
+(3, 'JetStar');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `airports`
+-- Cấu trúc bảng cho bảng `card`
 --
 
-CREATE TABLE IF NOT EXISTS `airports` (
-  `airport_id` int(11) NOT NULL,
-  `airport_name` varchar(55) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `airports`
---
-
-INSERT INTO `airports` (`airport_id`, `airport_name`) VALUES
-(1, 'Doha Hamad International Airport'),
-(2, 'Soekarno Hatta Intl Airport'),
-(3, 'Abu Dhabi Intl'),
-(4, 'Tan Son Nhat International Airport');
+CREATE TABLE `card` (
+  `id` int(11) NOT NULL,
+  `method` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `card_number` int(11) NOT NULL,
+  `card_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ccv_code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cities`
+-- Cấu trúc bảng cho bảng `flights_booking`
 --
 
-CREATE TABLE IF NOT EXISTS `cities` (
-  `city_id` int(11) NOT NULL,
-  `city_name` varchar(55) NOT NULL,
-  `city_code` varchar(15) NOT NULL,
-  `city_airport_id` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `cities`
---
-
-INSERT INTO `cities` (`city_id`, `city_name`, `city_code`, `city_airport_id`) VALUES
-(1, 'Hồ Chí Minh', 'HCM', 1),
-(2, 'Hà Nội', 'HAN', 2),
-(3, 'Đà Nẵng', 'DNA', 3);
+CREATE TABLE `flights_booking` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `flist_id` int(11) NOT NULL,
+  `fclass_id` int(11) NOT NULL,
+  `flight_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `total_per` int(11) NOT NULL,
+  `ticket_buy` datetime NOT NULL,
+  `total_price` double NOT NULL,
+  `card_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Cấu trúc bảng cho bảng `flight_class`
 --
 
-CREATE TABLE IF NOT EXISTS `customers` (
-  `customer_id` int(11) NOT NULL,
-  `customer_user_id` int(11) NOT NULL,
-  `customer_first_name` varchar(55) NOT NULL,
-  `customer_last_name` varchar(16) NOT NULL,
-  `customer_title` varchar(16) NOT NULL,
-  `customer_transfer` varchar(55) NOT NULL,
-  `customer_paypal` varchar(55) NOT NULL,
-  `customer_credit_card` int(16) NOT NULL,
-  `customer_credit_name` varchar(55) NOT NULL,
-  `customer_credit_ccv` varchar(8) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+CREATE TABLE `flight_class` (
+  `id` int(11) NOT NULL,
+  `fclass_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `customers`
+-- Đang đổ dữ liệu cho bảng `flight_class`
 --
 
-INSERT INTO `customers` (`customer_id`, `customer_user_id`, `customer_first_name`, `customer_last_name`, `customer_title`, `customer_transfer`, `customer_paypal`, `customer_credit_card`, `customer_credit_name`, `customer_credit_ccv`) VALUES
-(1, 0, '', '', '', '', '', 0, '', ''),
-(2, 0, '', '', '', '', '', 0, '', ''),
-(3, 0, '', '', '', '', '', 0, '', ''),
-(4, 0, '', '', '', '', '', 0, '', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `flight_booking`
---
-
-CREATE TABLE IF NOT EXISTS `flight_booking` (
-  `fb_id` int(11) NOT NULL,
-  `fb_airline_id` int(11) NOT NULL,
-  `fb_city_from_id` int(11) NOT NULL,
-  `fb_city_to_id` int(11) NOT NULL,
-  `fb_transit_id` int(11) NOT NULL,
-  `fb_customer_id` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `flight_booking`
---
-
-INSERT INTO `flight_booking` (`fb_id`, `fb_airline_id`, `fb_city_from_id`, `fb_city_to_id`, `fb_transit_id`, `fb_customer_id`) VALUES
-(1, 0, 0, 0, 0, 0),
-(2, 0, 0, 0, 0, 0),
-(3, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `flight_classes`
---
-
-CREATE TABLE IF NOT EXISTS `flight_classes` (
-  `fc_id` int(11) NOT NULL,
-  `fc_name` varchar(55) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `flight_classes`
---
-
-INSERT INTO `flight_classes` (`fc_id`, `fc_name`) VALUES
+INSERT INTO `flight_class` (`id`, `fclass_name`) VALUES
 (1, 'Economy'),
 (2, 'Business'),
 (3, 'Premium Economy');
@@ -161,218 +95,198 @@ INSERT INTO `flight_classes` (`fc_id`, `fc_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `flight_list`
+-- Cấu trúc bảng cho bảng `flight_list`
 --
 
-CREATE TABLE IF NOT EXISTS `flight_list` (
-  `fl_id` int(11) NOT NULL,
-  `fl_code` varchar(15) NOT NULL,
-  `fl_airline_id` int(11) NOT NULL,
-  `fl_fc_id` int(11) NOT NULL,
-  `fl_type` tinyint(11) NOT NULL,
-  `fl_city_from_id` int(11) NOT NULL,
-  `fl_city_to_id` int(11) NOT NULL,
-  `fl_departure_day` datetime DEFAULT NULL,
-  `fl_return_day` datetime DEFAULT NULL,
-  `fl_total` int(11) NOT NULL,
-  `fl_cost` float NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `flight_list` (
+  `id` int(11) NOT NULL,
+  `airways_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `place_from` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `airport_from` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `time_from` time NOT NULL,
+  `place_to` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `airport_to` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `time_to` time NOT NULL,
+  `duration` text COLLATE utf8_unicode_ci NOT NULL,
+  `transit` int(11) NOT NULL,
+  `total_sit` int(11) NOT NULL,
+  `code` varchar(55) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `flight_list`
+-- Đang đổ dữ liệu cho bảng `flight_list`
 --
 
-INSERT INTO `flight_list` (`fl_id`, `fl_code`, `fl_airline_id`, `fl_fc_id`, `fl_type`, `fl_city_from_id`, `fl_city_to_id`, `fl_departure_day`, `fl_return_day`, `fl_total`, `fl_cost`) VALUES
-(1, 'VVCS/VCS', 1, 2, 2, 1, 2, '2019-03-01 09:00:00', '2019-03-04 12:00:00', 100, 1500000),
-(2, 'VVCT/VCA', 2, 2, 1, 1, 3, '2019-03-06 15:00:00', '2019-03-07 15:00:00', 150, 5500000),
-(3, 'VVTS/HCM', 3, 3, 3, 2, 3, '2019-03-07 19:00:00', '2019-03-08 15:00:00', 250, 8500000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `nations`
---
-
-CREATE TABLE IF NOT EXISTS `nations` (
-  `nation_id` int(11) NOT NULL,
-  `nation_name` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `nations`
---
-
-INSERT INTO `nations` (`nation_id`, `nation_name`) VALUES
-(1, 'Việt Nam'),
-(2, 'Hàn Quốc'),
-(3, 'Nhật Bản');
+INSERT INTO `flight_list` (`id`, `airways_id`, `city_id`, `place_from`, `airport_from`, `time_from`, `place_to`, `airport_to`, `time_to`, `duration`, `transit`, `total_sit`, `code`) VALUES
+(1, 1, 1, 'TP Hồ Chí Minh (SNG)', 'Tan Son Nhat Airport', '18:45:00', 'Hà Nội (HAN)\r\n', 'Ha Noi Airport', '02:55:00', '11h 10m', 1, 1200000, 'VN 23434'),
+(2, 2, 2, 'Hà Nội (HAN)', 'Ha Noi Airport', '22:50:00', 'Đà Nẵng (DN)', 'Da Nang Airport', '10:50:00', '36h 00m', 2, 1300000, 'VN 23235'),
+(3, 3, 3, 'Đà Nẵng (DN)', 'Da Nang Airport', '05:11:00', 'Cần Thơ (CTN)', 'Can Tho Airport', '14:00:00', '9h 00m', 2, 1600000, 'VN 65453'),
+(4, 2, 4, 'Huế (HN)', 'Hue Airport', '13:17:12', 'Tp. Hồ Chí Minh', 'Tan Son Nhat Airport', '06:25:10', '7h 00m', 1, 1800000, 'VN 43353');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transits`
+-- Cấu trúc bảng cho bảng `list_cities`
 --
 
-CREATE TABLE IF NOT EXISTS `transits` (
-  `transit_id` int(11) NOT NULL,
-  `transit_city_id` int(11) NOT NULL,
-  `transit_fl_id` int(11) NOT NULL,
-  `transit_departure_date` datetime NOT NULL,
-  `transit_landing_date` datetime NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `list_cities` (
+  `id` int(11) NOT NULL,
+  `cityname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `shortname` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `transits`
+-- Đang đổ dữ liệu cho bảng `list_cities`
 --
 
-INSERT INTO `transits` (`transit_id`, `transit_city_id`, `transit_fl_id`, `transit_departure_date`, `transit_landing_date`) VALUES
-(1, 1, 1, '2019-03-08 02:05:00', '2019-03-07 00:05:00'),
-(2, 2, 2, '2019-03-08 09:00:00', '2019-03-08 08:10:00'),
-(3, 3, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `list_cities` (`id`, `cityname`, `shortname`) VALUES
+(1, 'Hồ Chí Minh', 'SGN'),
+(2, 'Hà Nội', 'HAN'),
+(3, 'Đà Nẵng', 'DN'),
+(4, 'Huế', 'HU'),
+(5, 'Bussan', 'KOREA'),
+(6, 'ToKio', 'JPN');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL,
-  `user_email` varchar(55) NOT NULL,
-  `user_password` varchar(128) NOT NULL,
-  `user_first_name` varchar(55) NOT NULL,
-  `user_last_name` varchar(15) NOT NULL,
-  `user_phone` varchar(15) NOT NULL,
-  `user_last_access` datetime DEFAULT NULL,
-  `user_attempt` int(11) DEFAULT NULL,
-  `user_status` tinyint(4) DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` int(13) NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `active` int(11) NOT NULL,
+  `attempt` int(11) NOT NULL,
+  `last_access` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_email`, `user_password`, `user_first_name`, `user_last_name`, `user_phone`, `user_last_access`, `user_attempt`, `user_status`) VALUES
-(7, 'nguyettran123@gmail.com', '$2y$10$blG8Vc7KDgEycpMHW8kRSuHDAtSJGgByrXqtYq9B1yKXg01w/yc6q', 'Tran Kim', 'Nguyet', '0123456789', '2019-03-13 01:44:12', 0, 1);
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `phone`, `remember_token`, `created_at`, `updated_at`, `active`, `attempt`, `last_access`) VALUES
+(15, 'dxhieu97@gmail.com', 'Xuân Hiếu', 'hieuit97', 999999, NULL, '2019-03-09 01:40:39', '2019-03-09 01:40:39', 0, 0, '0000-00-00 00:00:00'),
+(16, 'hieubds97@gmail.com', 'Đinh Xuân Hiếu', '$2y$10$.Ao5BTgmMssh.5RXF3xkLOQzeh3jblUPE/U.c.GXY22nFkdqkNuoy', 345995727, '65THgQjWfdqclq9MGNr9DPAwzra7LmYjZMdeBl54yPTbdEKi1kuFivhlsEEv', '2019-04-01 15:18:19', '2019-04-01 15:18:19', 0, 0, '0000-00-00 00:00:00');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `airlines`
+-- Chỉ mục cho bảng `airways`
 --
-ALTER TABLE `airlines`
-  ADD PRIMARY KEY (`airline_id`);
+ALTER TABLE `airways`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `airways_id` (`id`),
+  ADD KEY `airways_id_2` (`id`);
 
 --
--- Indexes for table `airports`
+-- Chỉ mục cho bảng `card`
 --
-ALTER TABLE `airports`
-  ADD PRIMARY KEY (`airport_id`);
+ALTER TABLE `card`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `cities`
+-- Chỉ mục cho bảng `flights_booking`
 --
-ALTER TABLE `cities`
-  ADD PRIMARY KEY (`city_id`);
+ALTER TABLE `flights_booking`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `flight_id` (`flist_id`),
+  ADD KEY `flights_booking_ibfk_1` (`user_id`),
+  ADD KEY `card_id` (`card_id`),
+  ADD KEY `fclass_id` (`fclass_id`);
 
 --
--- Indexes for table `customers`
+-- Chỉ mục cho bảng `flight_class`
 --
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`customer_id`);
+ALTER TABLE `flight_class`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `flight_booking`
---
-ALTER TABLE `flight_booking`
-  ADD PRIMARY KEY (`fb_id`);
-
---
--- Indexes for table `flight_classes`
---
-ALTER TABLE `flight_classes`
-  ADD PRIMARY KEY (`fc_id`);
-
---
--- Indexes for table `flight_list`
+-- Chỉ mục cho bảng `flight_list`
 --
 ALTER TABLE `flight_list`
-  ADD PRIMARY KEY (`fl_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `airways_id` (`airways_id`);
 
 --
--- Indexes for table `nations`
+-- Chỉ mục cho bảng `list_cities`
 --
-ALTER TABLE `nations`
-  ADD PRIMARY KEY (`nation_id`);
+ALTER TABLE `list_cities`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transits`
---
-ALTER TABLE `transits`
-  ADD PRIMARY KEY (`transit_id`);
-
---
--- Indexes for table `users`
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `airlines`
+-- AUTO_INCREMENT cho bảng `airways`
 --
-ALTER TABLE `airlines`
-  MODIFY `airline_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+ALTER TABLE `airways`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `airports`
+-- AUTO_INCREMENT cho bảng `card`
 --
-ALTER TABLE `airports`
-  MODIFY `airport_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+ALTER TABLE `card`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `cities`
+-- AUTO_INCREMENT cho bảng `flights_booking`
 --
-ALTER TABLE `cities`
-  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE `flights_booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT cho bảng `flight_class`
 --
-ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+ALTER TABLE `flight_class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `flight_booking`
---
-ALTER TABLE `flight_booking`
-  MODIFY `fb_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `flight_classes`
---
-ALTER TABLE `flight_classes`
-  MODIFY `fc_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `flight_list`
+-- AUTO_INCREMENT cho bảng `flight_list`
 --
 ALTER TABLE `flight_list`
-  MODIFY `fl_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `nations`
+-- AUTO_INCREMENT cho bảng `list_cities`
 --
-ALTER TABLE `nations`
-  MODIFY `nation_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE `list_cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT for table `transits`
---
-ALTER TABLE `transits`
-  MODIFY `transit_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `flights_booking`
+--
+ALTER TABLE `flights_booking`
+  ADD CONSTRAINT `flights_booking_ibfk_1` FOREIGN KEY (`flist_id`) REFERENCES `flight_list` (`id`),
+  ADD CONSTRAINT `flights_booking_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `flights_booking_ibfk_3` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`),
+  ADD CONSTRAINT `flights_booking_ibfk_4` FOREIGN KEY (`fclass_id`) REFERENCES `flight_class` (`id`);
+
+--
+-- Các ràng buộc cho bảng `flight_list`
+--
+ALTER TABLE `flight_list`
+  ADD CONSTRAINT `flight_list_ibfk_1` FOREIGN KEY (`airways_id`) REFERENCES `airways` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
